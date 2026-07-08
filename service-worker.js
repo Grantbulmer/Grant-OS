e5bdd823affeafdb9073df6ef3716b2fc965917d
Grantbulmer <Grantbulmer92@gmail.com>
2026-07-08 17:18:32 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grant-os-v283-housekeeping-cleanup';
+const CACHE_NAME = 'grant-os-v283-cashflow-warning-card';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon-32.png', './apple-touch-icon.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
